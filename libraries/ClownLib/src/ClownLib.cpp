3d3bbf36c7b33fa79cc15906bc91540587b4bfcd
Chase Barnes <chasebarn35@tamu.edu>
2021-12-29 12:29:43 -0600

    Moving stuff around

--- libraries/ClownLib/src/ClownLib.cpp
+++ libraries/ClownLib/src/ClownLib.cpp
@@ -1,2 +1,7 @@
+#include <ClownLib.h>
 
+
+void error_callback(int error, const char* description){
+    std::cout << "Error " << error << " { " << description << " }" << std::endl;
+}
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window, GLfloat& _X, GLfloat& _Y);
 void ShaderSuccess(GLuint type,std::string Name);
 
-const char *vertexShaderSource = "#version 400 compatibility\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char *fragmentShaderSource = "#version 400 compatibility\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\0";
-
-
-
 int main(int argc, char* argv[]){
 glfwSetErrorCallback(error_callback);
 
